12e1a31802e90d55461100e9612be3c3cff26008
voicelegion <Romans.Pipcenko@gmail.com>
2017-02-20 18:17:35 +0200

    Added more dependencies to POM, so Application could run. Reworked JSONService.java and App.java classes so imports work correctly (previously had fake address - by accident

--- src/main/java/lv/romanpipchenko/restexample/App.java
+++ src/main/java/lv/romanpipchenko/restexample/App.java
@@ -4,6 +4,7 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
+
 public class App {
 
     public static void main(String[] args) throws Exception {
